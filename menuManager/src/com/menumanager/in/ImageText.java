56fac87a22e0d1e69f9a4568dc0a212af7756cb4
Nithya <nithya@nithya-Latitude-D620.(none)>
2011-02-22 14:14:01 +0530

    first commit

---
+++ menuManager/src/com/menumanager/in/ImageText.java
@@ -0,0 +1,18 @@
+package com.menumanager.in;
+
+public class ImageText {
+	    private String imageUrl;
+	    private String text;
+	 
+	    public ImageText(String imageUrl, String text) {
+	        this.imageUrl = imageUrl;
+	        this.text = text;
+	    }
+	    public String getImageUrl() {
+	        return imageUrl;
+	    }
+	    public String getText() {
+	        return text;
+	    }
+	}
+
---
+++ menuManager/src/com/menumanager/in/MenuManager.java
@@ -0,0 +1,25 @@
+package com.menumanager.in;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+public class MenuManager extends Activity{
+	@Override
+	   public void onCreate(Bundle savedInstanceState)
+	   {
+	       super.onCreate(savedInstanceState);
+	    
+}
+}
